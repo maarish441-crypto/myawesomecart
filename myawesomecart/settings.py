33bb1010c0e71e4e2f81428afd8007e275cf817c
maarish441-crypto <maarish441@gmail.com>
2023-04-05 00:55:51 +0530

    ass

--- myawesomecart/settings.py
+++ myawesomecart/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-jg!-3+epmuu(qvb@(-hbdm2q8*&94fcyv&le43op2vs3860^sz"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 
 
 def index(request):
-    return render(request,'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4) - (n//4))
+
+    allProds = []
+    catProds = Product.objects.values('category','id')
+    cats = {item['category'] for item in catProds} 
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4) + (n//4))
+        allProds.append([prod,range(1,nSlides),nSlides])
+
+
+
+    params = {'allProds':allProds}
+    return render(request,'shop/index.html',params)
 
 def about(request):
     return  render(request,'shop/about.html')
